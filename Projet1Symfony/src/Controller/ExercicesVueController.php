ed20e35dfb0be8b68552b89e736e78c756d81b31
choquitofrito <zyriab@gmail.com>
2023-09-12 12:13:33 +0200

    add bootstrap

--- Projet1Symfony/src/Controller/ExercicesVueController.php
+++ Projet1Symfony/src/Controller/ExercicesVueController.php
@@ -12,12 +12,13 @@ class ExercicesVueController extends AbstractController
     #[Route('/exercices/vue/exercice1/{prix}/{tauxTva}')]
     public function exercice1(Request $req): Response
     {
-        $prix = $req->get ('prix');
-        $tauxTva = $req->get ('tauxTva');
-        $prixTotal = $prix + $prix * (1 + $tauxTva/100);
+        $prix = $req->get('prix');
+        $tauxTva = $req->get('tauxTva');
+        $prixTotal = $prix + $prix * (1 + $tauxTva / 100);
 
 
-        return $this->render('exercices_vue/exercice1.html.twig',
+        return $this->render(
+            'exercices_vue/exercice1.html.twig',
             ['prixTotal' => $prixTotal]
         );
     }
@@ -26,39 +27,65 @@ class ExercicesVueController extends AbstractController
     #[Route('/exercices/vue/exercice2/{prix}/{tauxTva}')]
     public function exercice2(Request $req): Response
     {
-        $prix = $req->get ('prix');
-        $tauxTva = $req->get ('tauxTva');
-        $prixTotal = $prix + $prix * (1 + $tauxTva/100);
+        $prix = $req->get('prix');
+        $tauxTva = $req->get('tauxTva');
+        $prixTotal = $prix + $prix * (1 + $tauxTva / 100);
 
-        return $this->render('exercices_vue/exercice2.html.twig',
-            ['prixTotal' => $prixTotal,
-            'prix' => $prix,
-            'tauxTva' => $tauxTva]
+        return $this->render(
+            'exercices_vue/exercice2.html.twig',
+            [
+                'prixTotal' => $prixTotal,
+                'prix' => $prix,
+                'tauxTva' => $tauxTva
+            ]
         );
     }
 
     #[Route('/exercices/vue/exercice2b/{prix}/{tauxTva}')]
     public function exercice2b(Request $req): Response
     {
-        $prix = $req->get ('prix');
-        $tauxTva = $req->get ('tauxTva');
+        $prix = $req->get('prix');
+        $tauxTva = $req->get('tauxTva');
 
-        return $this->render('exercices_vue/exercice2b.html.twig',
-            ['prix' => $prix,
-            'tauxTva' => $tauxTva]
+        return $this->render(
+            'exercices_vue/exercice2b.html.twig',
+            [
+                'prix' => $prix,
+                'tauxTva' => $tauxTva
+            ]
         );
     }
 
     #[Route('/exercices/vue/exercice3')]
-    public function exercice3 ():Response {
-        return $this->render ('exercices_vue/exercice3.html.twig');
-
+    public function exercice3(): Response
+    {
+        return $this->render('exercices_vue/exercice3.html.twig');
     }
 
 
+    // #[Route('/exercices/vue/exercice4')]
+    // public function exercice4(): Response
+    // {
+    //     return $this->render(
+    //         'exercices_vue/exercice4.html.twig',
+    //         [
+    //             'tabNoms' => ['Gent', 'Bruxelles', 'Dinant']
+    //         ]
+    //     );
+    // }
 
+    #[Route('/exercices/vue/exercice4')]
+    public function exercice4(): Response
+    {
+        $tabNoms = ['Gent', 'Bruxelles', 'Dinant'];
 
+        // ce qu'on envoie à la vue
+        $vars = ['tabNoms' => $tabNoms,
+                'salutation' => "Hi there "];
 
-
-
+        return $this->render(
+            'exercices_vue/exercice4.html.twig',
+            $vars
+        );
+    }
 }
