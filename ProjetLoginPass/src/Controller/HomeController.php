35cb7a09b630b75985ed35e025befe5e51864410
choquitofrito <zyriab@gmail.com>
2023-09-20 12:22:52 +0200

    inscription done

--- ProjetLoginPass/src/Controller/HomeController.php
+++ ProjetLoginPass/src/Controller/HomeController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController{
 
+    #[IsGranted('ROLE_USER')]
     #[Route ('/', name:'home')]
     public function home (){
         return $this->render ('home/accueil.html.twig');
--- ProjetLoginPass/src/Controller/SecurityController.php
+++ ProjetLoginPass/src/Controller/SecurityController.php
@@ -25,8 +25,9 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->render ("security/login.html.twig");
+        // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
