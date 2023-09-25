9ddb11c79126fdfdd454ce30bf16c6b31f703a7b
choquitofrito <zyriab@gmail.com>
2023-09-25 09:14:55 +0200

    base dql

--- ProjetFormulaires/src/Controller/ExamplesDqlController.php
+++ ProjetFormulaires/src/Controller/ExamplesDqlController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ExamplesDqlController extends AbstractController {
 
     #[Route('/dql/exemple1')]
-    public function dqlExemple1 (){
-        dd("Bonjour les wads!!");
+    public function dqlExemple1 (ManagerRegistry $doctrine){
+        
+        $em = $doctrine->getManager();
+        $query = $em->createQuery ("");
+        $res = $query->getResult();
+
+        dd($res);
+
 
     }
 
